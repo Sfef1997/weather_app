be63a3e9a4be9c778950e96485345c9906a69a45
mohanadsfef <>
2023-11-17 16:54:10 +0100

    new upload python weather app

---
+++ weather_app.py
@@ -0,0 +1,64 @@
+
+
+import tkinter as tk
+import requests
+
+api_key = '44e3a58014ada440483ed460f8644c42'
+
+def weather_gui():
+    def get_weather():
+        city_name = entry_search.get()
+        base_url = "https://api.openweathermap.org/data/2.5/weather"
+        params = {
+            "q": city_name,
+            "appid": api_key,
+            "units": "metric"
+        }
+        response = requests.get(base_url, params=params)
+        
+        if response.status_code == 200:
+            try:
+                weather_data = response.json()
+                temperature = weather_data['main']['temp']
+                humidity = weather_data['main']['humidity']
+                wind_speed = weather_data['wind']['speed']
+                pressure = weather_data['main']['pressure']
+                # Update labels with weather information
+                temp_label.config(text=f"Temperature: {temperature}°C")
+                humidity_label.config(text=f"Humidity: {humidity}%")
+                wind_label.config(text=f"Wind Speed: {wind_speed} km/h")
+                pressure_label.config(text=f"Pressure: {pressure} Pa")
+                error_label.config(text="")
+            except KeyError as e:
+                error_label.config(text=f"Error: {e}. Please try again.")
+        else:
+            error_label.config(text="Failed to fetch weather data. Please check your connection.")
+
+    window = tk.Tk()
+    window.title("Weather App")
+    window.geometry("600x800")
+    frame = tk.Frame(window)
+    frame.pack(padx=10, pady=10)
+  
+    label_get_location = tk.Label(frame, text="Enter City:")
+    label_get_location.grid(row=0, column=0)  
+    entry_search = tk.Entry(frame)
+    entry_search.grid(row=0, column=1, padx=5)
+    search_btn = tk.Button(frame, text="Search", padx=3, command=get_weather)
+    search_btn.grid(row=0, column=2)
+    
+    # Labels to display weather information
+    temp_label = tk.Label(frame, text="Temperature:")
+    temp_label.grid(row=3, column=0)
+    humidity_label = tk.Label(frame, text="Humidity:")
+    humidity_label.grid(row=4, column=0)
+    wind_label = tk.Label(frame, text="Wind Speed:")
+    wind_label.grid(row=5, column=0)
+    pressure_label = tk.Label(frame, text="Pressure:")
+    pressure_label.grid(row=6, column=0)
+
+    error_label = tk.Label(frame, text="", fg="red")
+    error_label.grid(row=7, column=0, columnspan=3)
+    window.mainloop()
+
+weather_gui()
